da8f376c1d61c1cc2b9db7651eccb9d084c9fa0f
jesse emerson <mrloquacious@gmail.com>
2021-02-01 13:40:09 -0800

    Add queries to DB setup script.

--- wb_schema.sql
+++ wb_schema.sql
@@ -1,3 +1,8 @@
+#To run this from the mariaDB prompt, use 'source wb_schema.sql'
+
+CREATE DATABASE WB;
+USE WB;
+
 CREATE TABLE TENKTUP1 (
     unique1 integer NOT NULL,
     unique2 integer NOT NULL PRIMARY KEY,
@@ -15,7 +20,7 @@ CREATE TABLE TENKTUP1 (
     stringu1 char(52) NOT NULL,
     stringu2 char(52) NOT NULL,
     string4  char(52) NOT NULL
-    )
+    );
 
 CREATE TABLE TENKTUP2 (
     unique1 integer NOT NULL,
@@ -34,7 +39,7 @@ CREATE TABLE TENKTUP2 (
     stringu1 char(52) NOT NULL,
     stringu2 char(52) NOT NULL,
     string4 char(52) NOT NULL
-    )
+    );
 
 CREATE TABLE ONEKTUP (
     unique1 integer NOT NULL,
@@ -53,4 +58,8 @@ CREATE TABLE ONEKTUP (
     stringu1 char(52) NOT NULL,
     stringu2 char(52) NOT NULL,
     string4 char(52) NOT NULL
-    )
+    );
+
+LOAD DATA LOCAL INFILE 'wb_data_10K.csv' INTO WB.TENKTUP1 FIELDS TERMINATED BY ',' ENCLOSED BY '\'' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE 'wb_data_10K.csv' INTO WB.TENKTUP2 FIELDS TERMINATED BY ',' ENCLOSED BY '\'' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE 'wb_data_1K.csv' INTO WB.ONEKTUP FIELDS TERMINATED BY ',' ENCLOSED BY '\'' LINES TERMINATED BY '\r\n';
